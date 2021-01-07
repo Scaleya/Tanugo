88adf6582b7c2cc068ba900e61ba2fe137aca767
Scaleya <61817665+Scaleya@users.noreply.github.com>
2021-01-07 15:15:18 +0800

    Update Td_GsnE.js

--- Td_GsnE.js
+++ Td_GsnE.js
@@ -1,20 +1,32 @@
-var fuckAdBlock = undefined, FuckAdBlock = undefined;
+// Function called if AdBlock is not detected
+function adBlockNotDetected() {
+    alert('AdBlock is not enabled');
+}
+// Function called if AdBlock is detected
+function adBlockDetected() {
+    alert('AdBlock is enabled');
+}
 
-var adBlockDetected = function() {
-            $('h1 span').text('yes');
-        }
-        var adBlockUndetected = function() {
-            $('h1 span').text('no');
-        }
-        if(typeof  FuckAdBlock === 'undefined') {
-            $(document).ready(adBlockDetected);
-        } else {
-            fuckAdBlock.on(true, adBlockDetected).on(false, adBlockUndetected);
-        }
-        // It removes the variable "fuckadblock" and "FuckAdBlock
-        // so that it not be exploited by another script later
-        // but beware, you can not use it too!
-        fuckAdBlock = undefined;
-        
-        // Do not use FuckAdBlock outside this area
-        // (between importing the script and the line above)
+// We look at whether FuckAdBlock already exists.
+if(typeof fuckAdBlock !== 'undefined' || typeof FuckAdBlock !== 'undefined') {
+    // If this is the case, it means that something tries to usurp are identity
+    // So, considering that it is a detection
+    adBlockDetected();
+} else {
+    // Otherwise, you import the script FuckAdBlock
+    var importFAB = document.createElement('script');
+    importFAB.onload = function() {
+        // If all goes well, we configure FuckAdBlock
+        fuckAdBlock.onDetected(adBlockDetected)
+        fuckAdBlock.onNotDetected(adBlockNotDetected);
+    };
+    importFAB.onerror = function() {
+        // If the script does not load (blocked, integrity error, ...)
+        // Then a detection is triggered
+        adBlockDetected(); 
+    };
+    importFAB.integrity = 'sha256-xjwKUY/NgkPjZZBOtOxRYtK20GaqTwUCf7WYCJ1z69w=';
+    importFAB.crossOrigin = 'anonymous';
+    importFAB.src = 'https://cdnjs.cloudflare.com/ajax/libs/fuckadblock/3.2.1/fuckadblock.min.js';
+    document.head.appendChild(importFAB);
+}
